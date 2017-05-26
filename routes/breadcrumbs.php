18d0fb415b14fc9e7a630b9c23c3d56542f34cff
Slava Rudkovskiy <xraskolx@gmail.com>
2017-05-26 12:04:17 +0300

    Cleanup

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,22 +2,15 @@
 
 use DaveJamesMiller\Breadcrumbs\Generator;
 
-Breadcrumbs::register('admin.users', function (Generator $breadcrumbs) {
-    $breadcrumbs->push(__('views.admin.dashboard.title'), route('admin.dashboard'));
-    $breadcrumbs->push(__('views.admin.users.index.title'));
+Breadcrumbs::register('home', function (Generator $breadcrumbs) {
 });
 
-Breadcrumbs::register('admin.users.show', function (Generator $breadcrumbs, \App\Models\Auth\User\User $user) {
-    $breadcrumbs->push(__('views.admin.dashboard.title'), route('admin.dashboard'));
-    $breadcrumbs->push(__('views.admin.users.index.title'), route('admin.users'));
-    $breadcrumbs->push(__('views.admin.users.show.title', ['name' => $user->name]));
+Breadcrumbs::register('try_and_buy', function (Generator $breadcrumbs, \App\Models\Auth\User\User $user) {
+
 });
 
+Breadcrumbs::register('subscription', function (Generator $breadcrumbs, \App\Models\Auth\User\User $user) {
 
-Breadcrumbs::register('admin.users.edit', function (Generator $breadcrumbs, \App\Models\Auth\User\User $user) {
-    $breadcrumbs->push(__('views.admin.dashboard.title'), route('admin.dashboard'));
-    $breadcrumbs->push(__('views.admin.users.index.title'), route('admin.users'));
-    $breadcrumbs->push(__('views.admin.users.edit.title', ['name' => $user->name]));
 });
 
 
