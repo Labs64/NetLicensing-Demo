6c3246172fec8977f001e9ee6241bbd965bcbf73
Slava Rudkovskiy <xraskolx@gmail.com>
2017-06-05 11:24:03 +0300

    Issue #1

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Mail\MailServiceProvider::class,
 
         /*
          * Package Service Providers...
