cab5d89cc1c707285a0b13f768ed0f351ee38e0c
Slava Rudkovskiy <xraskolx@gmail.com>
2017-06-06 15:26:07 +0300

    Remove bower

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,52 +29,52 @@ mix.webpackConfig({
 
 mix.scripts([
     'node_modules/jquery/dist/jquery.js',
-    'bower_components/PACE/pace.js',
-    'bower_components/bootstrap/dist/js/bootstrap.js',
-    'bower_components/gentelella/build/js/custom.js',
+    'node_modules/pace-progress/pace.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/gentelella/build/js/custom.js',
     'resources/assets/js/default.js'
 ], 'public/assets/js/default.js').version();
 
 mix.styles([
-    'bower_components/font-awesome/css/font-awesome.css',
-    'bower_components/PACE/themes/blue/pace-theme-minimal.css',
-    'bower_components/bootstrap/dist/css/bootstrap.css',
-    'bower_components/gentelella/vendors/animate.css/animate.css',
-    'bower_components/gentelella/build/css/custom.css',
+    'node_modules/font-awesome/css/font-awesome.css',
+    'node_modules/pace-progress/themes/blue/pace-theme-minimal.css',
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/gentelella/vendors/animate.css/animate.css',
+    'node_modules/gentelella/build/css/custom.css',
     'resources/assets/css/default.css'
 ], 'public/assets/css/default.css').version();
 
 mix.copy([
-    'bower_components/font-awesome/fonts/',
-    'bower_components/gentelella/vendors/bootstrap/dist/fonts'
+    'node_modules/font-awesome/fonts/',
+    'node_modules/gentelella/vendors/bootstrap/dist/fonts'
 ], 'public/assets/fonts');
 
 mix.scripts([
-    'bower_components/SyntaxHighlighter/scripts/XRegExp.js',
-    'bower_components/SyntaxHighlighter/scripts/shCore.js',
-    'bower_components/SyntaxHighlighter/scripts/shBrushXml.js',
-    'bower_components/SyntaxHighlighter/scripts/shBrushPhp.js',
-    'bower_components/FooTable/compiled/footable.min.js',
+    'node_modules/syntaxhighlighter/scripts/XRegExp.js',
+    'node_modules/syntaxhighlighter/scripts/shCore.js',
+    'node_modules/syntaxhighlighter/scripts/shBrushXml.js',
+    'node_modules/syntaxhighlighter/scripts/shBrushPhp.js',
+    'node_modules/footable/compiled/footable.min.js',
     'resources/assets/js/try_and_buy.js'
 ], 'public/assets/js/try_and_buy.js').version();
 
 mix.styles([
-    'bower_components/SyntaxHighlighter/styles/shCore.css',
-    'bower_components/SyntaxHighlighter/styles/shThemeDefault.css',
-    'bower_components/FooTable/compiled/footable.bootstrap.min.css'
+    'node_modules/syntaxhighlighter/styles/shCore.css',
+    'node_modules/syntaxhighlighter/styles/shThemeDefault.css',
+    'node_modules/footable/compiled/footable.bootstrap.min.css'
 ], 'public/assets/css/try_and_buy.css').version();
 
 mix.scripts([
-    'bower_components/SyntaxHighlighter/scripts/XRegExp.js',
-    'bower_components/SyntaxHighlighter/scripts/shCore.js',
-    'bower_components/SyntaxHighlighter/scripts/shBrushXml.js',
-    'bower_components/SyntaxHighlighter/scripts/shBrushPhp.js',
-    'bower_components/FooTable/compiled/footable.min.js',
+    'node_modules/syntaxhighlighter/scripts/XRegExp.js',
+    'node_modules/syntaxhighlighter/scripts/shCore.js',
+    'node_modules/syntaxhighlighter/scripts/shBrushXml.js',
+    'node_modules/syntaxhighlighter/scripts/shBrushPhp.js',
+    'node_modules/footable/compiled/footable.min.js',
     'resources/assets/js/subscription.js'
 ], 'public/assets/js/subscription.js').version();
 
 mix.styles([
-    'bower_components/SyntaxHighlighter/styles/shCore.css',
-    'bower_components/SyntaxHighlighter/styles/shThemeDefault.css',
-    'bower_components/FooTable/compiled/footable.bootstrap.min.css'
+    'node_modules/syntaxhighlighter/styles/shCore.css',
+    'node_modules/syntaxhighlighter/styles/shThemeDefault.css',
+    'node_modules/footable/compiled/footable.bootstrap.min.css'
 ], 'public/assets/css/subscription.css').version();
