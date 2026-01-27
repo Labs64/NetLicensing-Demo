84bb57638a5f4b18007e33fa3ab02ff30f464dcd
Konstantin Korotkov <konstantin.korotkov@labs64.de>
2026-01-27 17:26:03 +0100

    Reanimate project in docker with legacy deps (#52)
    Co-authored-by: Viacheslav Rudkovskyi <xraskolx@gmail.com>

--- dockerfiles/bin/prj-build.sh
+++ dockerfiles/bin/prj-build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+set -e
+
 # Install dependencies
-composer install --prefer-dist --no-interaction
+composer install --prefer-dist --no-interaction --no-security-blocking
 
 # Generate application key
 php artisan key:generate
+php artisan config:clear
 
 # Verify environment config
 cat .env
 
 # Install dependencies
-npm install
+npm ci
 npm run dev
 
 # Execute PHPUnit tests
